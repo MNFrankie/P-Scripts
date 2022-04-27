0b21ee10d9af8b431e8d53fbc236cbe0100b712a
Francis Nyagah <francismuriithi15@gmail.com>
2022-04-27 08:45:30 +0300

    Update pythonscript32.py

--- pythonscript32.py
+++ pythonscript32.py
@@ -17,7 +17,7 @@ tn.write(b"enable\n")
 tn.write(b"cisco\n")
 tn.write(b"configure terminal\n")
 
-for n in range (2, 11):
+for n in range (2,11):
         tn.write(b"vlan " + str(n) + "\n")
         tn.write(b"name Python_VLAN_" + str(n) + "\n")
 
