30a69fd99d8a2b4ada91db22b52f54bef3cfd36f
Francis Nyagah <francismuriithi15@gmail.com>
2022-04-27 11:19:48 +0300

    Update pythonscript33.py

--- pythonscript33.py
+++ pythonscript33.py
@@ -9,9 +9,7 @@ for IP in f:
     HOST = IP
     user = input("Enter your Telnet Username: ")
     password = getpass.getpass()
-
     tn = telnetlib.Telnet(HOST)
-
     tn.read_until(b"Username: ")
     tn.write(user.encode('ascii') + b"\n")
     if password:
@@ -20,14 +18,11 @@ for IP in f:
     tn.write(b"enable\n")
     tn.write(b"cisco\n")
     tn.write(b"configure terminal\n")
-
 for n in range (2,110):
     tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
     tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
-
 tn.write(b"end\n")
 tn.write(b"exit\n")
-
 print(tn.read_all().decode('ascii'))
 
 
