5e0c74d4402a49ab3e42b9966b40d857336947be
Francis Nyagah <francismuriithi15@gmail.com>
2022-04-27 08:30:19 +0300

    Create pythonscript32.py
    Introduced Loop

---
+++ pythonscript32.py
@@ -0,0 +1,29 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.45.71"
+user = input("Enter your Telnet Username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"configure terminal\n")
+
+    for n in range (2,11):
+        tn.write("vlan " + str(n) + "\n")
+        tn.write("name Python_VLAN_" + str(n) + "\n")
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
+
+
