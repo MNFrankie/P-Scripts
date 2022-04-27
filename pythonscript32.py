ce5b4c43cfd2dfe324fd30328e9bf3612ce11b11
Francis Nyagah <francismuriithi15@gmail.com>
2022-04-27 09:07:50 +0300

    Update pythonscript32.py
    fixed spelling of format

--- pythonscript32.py
+++ pythonscript32.py
@@ -12,14 +12,13 @@ tn.write(user.encode('ascii') + b"\n")
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
-
-tn.write(b"enable\n")
-tn.write(b"cisco\n")
-tn.write(b"configure terminal\n")
+    tn.write(b"enable\n")
+    tn.write(b"cisco\n")
+    tn.write(b"configure terminal\n")
 
 for n in range (2,11):
-        tn.write(b"vlan " + str(n) + "\n")
-        tn.write(b"name Python_VLAN_" + str(n) + "\n")
+    tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+    tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
 
 tn.write(b"end\n")
 tn.write(b"exit\n")
